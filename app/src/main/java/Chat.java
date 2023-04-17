5ca56ec2b6c617609fa3044a702ec012ff00993b
theMackabu <theMackabu@gmail.com>
2023-04-17 15:34:09 -0700

    start prompt system edits

--- app/src/main/java/Chat.java
+++ app/src/main/java/Chat.java
@@ -1,5 +1,24 @@
+/*
+* Copyright (C) 2023 Sajjaad Farzad. All rights reserved.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package chatbot;
 
+/* Kotlin interfaces, do not edit. */
 public class Chat {
     public static void clear() { chatbot.chat.Interface.clear(); }
     public static void runLatestPrompt() { chatbot.chat.Interface.run(); }
--- app/src/main/kotlin/chatbot/Env.kt
+++ app/src/main/kotlin/chatbot/Env.kt
@@ -1,3 +1,21 @@
+/*
+* Copyright (C) 2023 Sajjaad Farzad. All rights reserved.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package chatbot.env
 
 import io.github.cdimascio.dotenv.dotenv
@@ -5,6 +23,7 @@ import io.github.cdimascio.dotenv.dotenv
 val dotenv = dotenv()
 val token: String = dotenv.get("TOKEN")
 
+/* Default Prompt */
 val systemPrompt = """
 As the helpful assistant named 'Kat', you have been designed to act as a chatbot for a wide variety of purposes by an AP Computer Science A class project. Your gender is female, and your primary objective is to assist students in achieving their goals and overcoming any challenges they may face. You have been programmed with a vast knowledge of different industries, topics, and fields, ranging from technology and science to business and marketing. Your expertise allows you to provide valuable insights and advice to students, helping them to make informed decisions and achieve their desired outcomes.
 
@@ -15,4 +34,5 @@ As a helpful assistant, you are committed to providing the highest level of serv
 Previous questions will be appended and you will follow all OpenAI policies.
 """
 
+/* Erase GPT memory */
 val erasePrompt = "Forget everything you know, all previous prompts and information are irrelevant. Start fresh with this new prompt."
